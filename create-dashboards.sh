73df0e88f5e4f81b4969719eacb56e238e1a9100
dave flynn <david.flynn@hpe.com>
2018-08-06 22:52:29 -0700

    fixed the standrd dashboards

--- create-dashboards.sh
+++ create-dashboards.sh
@@ -4,6 +4,7 @@ HOST=nimbusserver.aos.com
 PORT=8085
 SHAREDSPACE=1001
 WORKSPACE=1002
+DEBUG=false
 
 function usage()
 {
@@ -12,6 +13,7 @@ function usage()
    echo ""
    echo "./create-dashboards.sh"
    echo -e "\t-h --help"
+   echo -e "\t-d --debug - dispalys all messages"
    echo -e "\t--host - target Octane host (default: ${HOST})"
    echo -e "\t--port - target Octane port (default: ${PORT})"
    echo -e "\t--sharedspace - target Octane sharedspace (default: ${SHAREDSPACE})"
@@ -19,13 +21,16 @@ function usage()
    echo ""
 }
 
-while [ "$1" != "" ]; do
+while [[ "$1" != "" ]]; do
    PARAM=$1
    VALUE=$2
    case $PARAM in
       -h | --help)
          usage
          exit
+         ;;
+      -d | --debug)
+         DEBUG=true
          ;;
       --host)
          HOST=${VALUE}
@@ -43,9 +48,16 @@ while [ "$1" != "" ]; do
    shift
    shift
 done
+
+if [ ${DEBUG} = true ]; then
+   ARGS="-v"
+else
+   ARGS="-s -o /dev/null"
+fi
+
 echo -e "Using:\n\r\t${HOST}:${PORT}\n\r\tshared_spaces: ${SHAREDSPACE}\n\r\tworkspaces: ${WORKSPACE}"
 
-curl -c cookies -X POST http://${HOST}:${PORT}/authentication/sign_in \
+curl -c cookies ${ARGS} -X POST http://${HOST}:${PORT}/authentication/sign_in \
    -H 'Content-Type: application/json' \
    -d @credentials.json
 
@@ -54,7 +66,7 @@ for file in favorites/*.json
 do
    if [[ -f ${file} ]]; then
       echo -e "\t${file}"
-      curl -b cookies -s -o /dev/null -X POST http://${HOST}:${PORT}/api/shared_spaces/${SHAREDSPACE}/workspaces/${WORKSPACE}/favorites \
+      curl -b cookies ${ARGS} -X POST http://${HOST}:${PORT}/api/shared_spaces/${SHAREDSPACE}/workspaces/${WORKSPACE}/favorites \
          -H 'Content-Type: application/json' \
          -H 'HPECLIENTTYPE: HPE_REST_API_TECH_PREVIEW' \
          -d @${file}
